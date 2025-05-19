8333b8677b70c6f686c7fdba723943fb0519701b
Blackburn0 <hamzatadebayo5@gmail.com>
2025-05-19 15:07:10 +0100

    Added basic_operation.py with addition, subtraction and multiplication code

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+print(f"Addintion of {number1} and {number2} is {result}")
+
+number1 = 10
+number2 = 5
+result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {result}")
+
+number1 = 10
+number2 = 5
+result = number1 + number2
+print(f"Multiplication of {number1} and {number2} is {result}")
+
